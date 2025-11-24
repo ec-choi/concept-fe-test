c622e58198017e7ac9fb1a507f0056580bf07378
ec-choi <choi.office91@gmail.com>
2025-11-24 23:24:39 +0900

    feat: 네비 위젯 추가

--- src/widgets/Nav/Nav.style.ts
+++ src/widgets/Nav/Nav.style.ts
@@ -1,16 +1,72 @@
 import { css } from '@emotion/react';
+import { color } from '@/shared/styles/color';
+import { fontWeight, typo } from '@/shared/styles/typo';
 
-export const studentsStyle = css`
-  width: 200px;
-  height: 225px;
-  border-radius: 4px;
+export const scrollRootStyle = css`
+  width: 192px;
+  height: 100%;
   overflow: hidden;
-  box-shadow: 0 2px 10px var(--black-a4);
-  background-color: white;
+  border: 1px solid ${color.gray400};
+  background: ${color.white};
+  border-radius: 12px;
   --scrollbar-size: 10px;
 `;
 
-export const studentsViewportStyle = css`
+export const scrollViewportStyle = css`
   width: 100%;
   height: 100%;
+`;
+
+export const scrollbarStyle = css`
+  display: flex;
+  user-select: none;
+  touch-action: none;
+  padding: 2px;
+  // background: ${color.gray100};
+  transition: background 160ms ease-out;
+  &:hover {
+    background: ${color.gray200};
+  }
+  &[data-orientation='vertical'] {
+    width: var(--scrollbar-size);
+  }
+  &[data-orientation='horizontal'] {
+    flex-direction: column;
+    height: var(--scrollbar-size);
+  }
+`;
+
+export const thumbStyle = css`
+  position: relative;
+  flex: 1;
+  background: ${color.gray400};
+  border-radius: var(--scrollbar-size);
+  &::before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+    min-width: 44px;
+    min-height: 44px;
+  }
+`;
+
+export const navButtonStyle = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 56px;
+  padding: 0 16px;
+  background: ${color.white};
+  ${typo.body2}
+  + .nav-item {
+    border-top: 1px solid ${color.gray400};
+  }
+  &[aria-current='page'] {
+    color: ${color.sky400};
+    ${fontWeight.bold}
+  }
 `;
--- src/widgets/Nav/Nav.tsx
+++ src/widgets/Nav/Nav.tsx
@@ -1,16 +1,33 @@
+import { ROOT_WITH_STUDENT_ID } from '@/app/routes/router';
 import type { Student } from '@/entities/student/model/types';
-import { studentsStyle, studentsViewportStyle } from '@/widgets/Nav/Nav.style';
+import {
+  navButtonStyle,
+  scrollRootStyle,
+  scrollViewportStyle,
+  scrollbarStyle,
+  thumbStyle,
+} from '@/widgets/Nav/Nav.style';
 import * as ScrollArea from '@radix-ui/react-scroll-area';
+import { generatePath, NavLink } from 'react-router';
 
-export const Students = ({ students }: { students: Student[] }) => {
-  console.log(students);
+export const Nav = ({ students }: { students: Student[] }) => {
   return (
-    <ScrollArea.Root css={studentsStyle} className="students">
-      <ScrollArea.Viewport css={studentsViewportStyle}>
+    <ScrollArea.Root css={scrollRootStyle} className="nav">
+      <ScrollArea.Viewport css={scrollViewportStyle}>
         {students.map((student) => (
-          <div key={student.id}>{student.name}</div>
+          <NavLink
+            key={student.id}
+            className="nav-item"
+            css={navButtonStyle}
+            to={generatePath(ROOT_WITH_STUDENT_ID, { studentId: student.id })}
+          >
+            {student.name}
+          </NavLink>
         ))}
       </ScrollArea.Viewport>
+      <ScrollArea.Scrollbar orientation="vertical" css={scrollbarStyle}>
+        <ScrollArea.Thumb css={thumbStyle} />
+      </ScrollArea.Scrollbar>
     </ScrollArea.Root>
   );
 };
