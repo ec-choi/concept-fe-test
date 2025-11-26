2d1e09d93f54ba785c38798af7ea474a7f6ee119
ec-choi <choi.office91@gmail.com>
2025-11-27 00:08:23 +0900

    feat: 스위치 스타일 적용

--- src/shared/ui/Switch/Switch.style.ts
+++ src/shared/ui/Switch/Switch.style.ts
@@ -2,31 +2,26 @@ import { css } from '@emotion/react';
 import { color } from '@/shared/styles/color';
 
 export const switchRootStyle = css`
-  width: 42px;
-  height: 25px;
-  background-color: ${color.gray500};
+  width: 40px;
+  height: 20px;
+  background-color: ${color.gray100};
   border-radius: 9999px;
-  position: relative;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-  border: none;
+  border: 1px solid ${color.gray400};
   cursor: pointer;
-  &[data-state='checked'] {
-    background-color: ${color.gray900};
-  }
 `;
 
 export const switchThumbStyle = css`
   display: block;
-  width: 21px;
-  height: 21px;
-  background-color: ${color.white};
+  width: 20px;
+  height: 20px;
+  background-color: ${color.gray500};
+  margin-top: -1px;
   border-radius: 9999px;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
   transition: transform 100ms;
-  transform: translateX(2px);
+  transform: translateX(0px);
   will-change: transform;
   &[data-state='checked'] {
     transform: translateX(19px);
+    background-color: ${color.sky400};
   }
 `;
