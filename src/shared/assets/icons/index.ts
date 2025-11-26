5ec849c4b2772bb28d9520c5348fcb03f99b150e
ec-choi <choi.office91@gmail.com>
2025-11-27 00:42:15 +0900

    feat: 체크박스 스타일

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -3,3 +3,5 @@ export { default as icon_sad } from './sad.svg?react';
 export { default as icon_smile } from './smile.svg?react';
 export { default as icon_star } from './star.svg?react';
 export { default as icon_arrow_down } from './arrow-down.svg?react';
+export { default as icon_checked } from './checked.svg?react';
+export { default as icon_dashed } from './dashed.svg?react';
--- src/shared/ui/Checkbox/Checkbox.style.ts
+++ src/shared/ui/Checkbox/Checkbox.style.ts
@@ -3,24 +3,23 @@ import { color } from '@/shared/styles/color';
 
 export const checkboxRootStyle = css`
   background-color: ${color.white};
-  width: 25px;
-  height: 25px;
+  width: 20px;
+  height: 20px;
   border-radius: 4px;
   display: flex;
   align-items: center;
   justify-content: center;
-  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
   border: 1px solid ${color.gray500};
   cursor: pointer;
-  &:hover {
-    background-color: ${color.gray100};
-  }
-  &:focus {
-    box-shadow: 0 0 0 2px ${color.gray900};
+
+  &[data-state='indeterminate'] {
+    background-color: ${color.sky400};
+    border-color: ${color.sky400};
+    color: ${color.white};
   }
   &[data-state='checked'] {
-    background-color: ${color.gray900};
-    border-color: ${color.gray900};
+    background-color: ${color.sky400};
+    border-color: ${color.sky400};
     color: ${color.white};
   }
 `;
